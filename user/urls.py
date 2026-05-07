2067d39e6abc2427f256d4d1a0aa8c9defe7eab8
unknown <kgoncharenko83@gmail.com>
2026-05-07 16:14:57 +0200

    refactor: refacting for 'cleancode'

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from user.views import CreateUserView, ManageUserView
 from user.views import CreateTokenView
 
-
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
     path("login/", CreateTokenView.as_view(), name="login"),
--- user/views.py
+++ user/views.py
@@ -1,6 +1,5 @@
 from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from user.serializers import UserSerializer
